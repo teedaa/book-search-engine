6d92210bf0a78c1b389632ea2beac9e1da7bea16
teedaa <pengtedachengllhs@gmail.com>
2022-12-13 18:49:52 -0800

    put apollo where it belongs

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/react-hooks'
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
